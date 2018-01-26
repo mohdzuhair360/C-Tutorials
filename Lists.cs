aac7aad089fe682120c8ed6f1e742c00df04398a
Zuhair Radzi <32242267+mohdzuhair360@users.noreply.github.com>
2018-01-26 16:55:08 +0800

    Lists 26/1

--- Lists.cs
+++ Lists.cs
@@ -23,6 +23,66 @@ namespace ConsoleApp2
             //add array to list
             int[] array1 = { 2, 4, 6, 8 };
             numList.AddRange(array1);
+
+            //print list
+            numList.ForEach(Console.WriteLine);
+            /*for (int i = 0; i < numList.Capacity; i++)
+            {
+                Console.WriteLine(numList[i]);
+            }*/
+
+            //clear list - numList.Clear();
+            Console.WriteLine();
+
+            //declare array in list
+            List<int> numlist2 = new List<int>(array1);
+            numlist2.ForEach(Console.WriteLine);
+            Console.WriteLine();
+
+            //delare array in list #2
+            List<int> numlist3 = new List<int>(new int[] { 1, 2, 3, 4, 5 });
+            numlist3.ForEach(Console.WriteLine);
+            Console.WriteLine();
+
+            //insert element of list at specific index (index, new element)
+            numList.Insert(0, 505);
+            numList.ForEach(Console.WriteLine);
+            Console.WriteLine();
+
+            //remove element of list at specified index (index)
+            //remove element from initial list
+            numList.RemoveAt(2);
+            numList.ForEach(Console.WriteLine);
+            Console.WriteLine();
+
+            //print out element in specified index
+            Console.WriteLine("5 is on index : " +numlist3.IndexOf(0));
+            Console.WriteLine();
+
+            //check if list contain the element
+            Console.WriteLine("1 in List or not? " + numlist3.Contains(1));
+            Console.WriteLine();
+
+            //list string
+
+            List<string> strList1 = new List<string>(new string[] { "Jon", "Igrite", "Cersei" });
+
+            //check for element in list
+            //string comparer ordinal ignore case to ignore capital/small letter
+            Console.WriteLine("Cersei is on index? : " + strList1.Contains("cersei", StringComparer.OrdinalIgnoreCase));
+            Console.WriteLine();
+
+            //sort list
+
+            //alphabetically
+            strList1.Sort();
+            strList1.ForEach(Console.WriteLine);
+
+            strList1.Sort();
+            strList1.ForEach(Console.WriteLine);
+
+
+
         }
     }
 

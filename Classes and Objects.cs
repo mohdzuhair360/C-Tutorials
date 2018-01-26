cf2eb4f96fd34db1437d18d0799a000d1a9e83a2
Zuhair Radzi <32242267+mohdzuhair360@users.noreply.github.com>
2018-01-27 01:20:25 +0800

    Exception Handling 27/1

---
+++ Classes and Objects.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class House
+
+    {
+        //public - not protected
+        //private - protected class, limited access to specific class
+
+        //automatically set getters and setters
+        public double height { get; set; }
+        public double weight { get; set; }
+        public string sigil { get; set; }
+        public string person { get; set; }
+
+        //manual getters and setters
+        //value is default
+        public string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        //default constructor
+        public House()
+        {
+            this.height = 0;
+            this.weight = 0;
+            this.name = "No Name";
+            this.sigil = "No sigil";
+            this.person = "No person";
+            NumOfHouses++;
+        }
+
+        //constructor to assign specific attributes to be passed
+        public House(double height, double weight, string name, string sigil, string person)
+        {
+            this.height = height;
+            this.weight = weight;
+            this.name = name;
+            this.sigil = sigil;
+            this.person = person;
+            NumOfHouses++;
+
+        }
+        //static class define static members
+
+        static int NumOfHouses = 0;
+        
+        public static int getNumofHouses()
+        {
+            return NumOfHouses;
+        }
+
+        public string toString()
+        {
+            return String.Format("{0} is from house {1} has sigil {2} and height is {3} and weight is {4}", person, name, sigil, height, weight);
+        }
+
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+
+}
--- Exception Handling.cs
+++ Exception Handling.cs
@@ -10,7 +10,27 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            
+            try
+            {
+                Console.Write("Divide 10 by ");
+                int num = int.Parse(Console.ReadLine());
+                Console.WriteLine("10/{0} = {1}", num, (10 / num));
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Can't divide by zero");
+                //return type of error
+                Console.WriteLine(ex.GetType().Name);
+                //return the message of error
+                Console.WriteLine(ex.Message);
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.GetType().Name);
+                Console.WriteLine(ex.Message);
+
+            }
 
         }
     }
